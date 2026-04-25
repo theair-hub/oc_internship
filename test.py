ac5790296f271ba6ab7ac3988dce6283f689973c
Ilaria <ilaria.dedominicis2@studio.unibo.it>
2026-04-25 16:06:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,49 @@
 from enricher_support import EnricherSupport
+import argparse
 
-def main():
-    csv_path = r"..."  
-    base_iri = "https://w3id.org/oc/meta/"
-    enricher = EnricherSupport(csv_zip_path=csv_path, base_iri=base_iri)
+# Peroni ha detto di usare argparse 
+parser = argparse.ArgumentParser(description="Arricchimento bibliografico con OpenCitations")
 
-    # carica file già processati (opzionale)
-    enricher.load_processed_files()
+parser.add_argument("--csv_path", default=r"C:\\Users\\ilari\\Desktop\\OpenCitations\\output_csv_2026_01_14")
+parser.add_argument("--base-iri", default="https://w3id.org/oc/meta/")
+parser.add_argument("--num-csv", type=int, default=31)
+parser.add_argument("--test-limit", type=int, default=None)
+parser.add_argument("--batch-size", type=int, default=5)
+parser.add_argument("--max-retries", type=int, default=5)
+parser.add_argument("--retry-delay", type=int, default=30)
 
-    # estrae OMID + identificatori dal CSV
-    enricher.extract_ids_from_csv(num_csv=1) # numero di CSV opzionale
+args = parser.parse_args()
 
-    # costruisce BR e Identifiers
-    enricher.build_graphset()
+enricher = EnricherSupport(csv_zip_path=args.csv_path, base_iri=args.base_iri)
 
-    # arricchisce e salva 
-    g_set = enricher.enrich(
-        enriched_file="enriched.rdf",
-        incomplete_file="incomplete.ttl"
-    )
+enricher.process_folder_streaming(
+    num_csv=args.num_csv,
+    test_limit=args.test_limit,
+    batch_size=args.batch_size
+)
 
-    # print
-    print(f"\nTotale BR creati: {enricher.created_br}")
-    print(f"BR/ID mancanti o con errori: {len(enricher.missing_data)}")
-    if enricher.missing_data:
-        print(enricher.missing_data[:5])
+print(f"\nTotale BR creati: {enricher.created_br}")
+print(f"BR/ID mancanti o con errori: {len(enricher.missing_data)}")
+if enricher.missing_data:
+    print(enricher.missing_data[:5])
 
-if __name__ == "__main__":
-    main()
+# errore di Wikidata: troppe richieste [GraphEnricher-WikiData]:JSONDecodeError('Expecting value: line 1 column 1 (char 0)')__
+"""         SELECT ?item WHERE {
+              ?item p:P356 ?x.
+              ?x ps:P356 "10.22435/BPK.V46I1.48".
+        } LIMIT 1
+        __text/html; charset=utf-8                                                     
+[GraphEnricher-WikiData]:JSONDecodeError('Expecting value: line 1 column 1 (char 0)')__
+        SELECT ?item WHERE {
+              ?item p:P356 ?x.
+              ?x ps:P356 "10.14238/PI56.5.2016.304-10".
+        } LIMIT 1
+        __text/html; charset=utf-8                                                     
+[GraphEnricher-WikiData]:JSONDecodeError('Expecting value: line 1 column 1 (char 0)')__
+        SELECT ?item WHERE {
+              ?item p:P356 ?x.
+              ?x ps:P356 "10.22435/HSJI.V12I2.2239".
+        } LIMIT 1
+        __text/html; charset=utf-8                                                     
+[GraphEnricher-WikiData]:JSONDecodeError('Expecting value: line 1 column 1 (char 0)')__
+ """
